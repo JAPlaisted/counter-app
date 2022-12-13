dd9fdf105f7cfa37f91fae0bd4274e40e68ee428
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2022-12-13 12:30:26 -0800

    Initial commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        count: 0,
+    };
+    render() { 
+        return (
+            <div>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
+            </div>
+        );
+    }
+    handleIncrement = () => {
+        this.setState({ count: this.state.count + 1})
+    }
+    getBadgeClasses() {
+        let badgeClasses = "badge m-2 badge-" + (this.state.count === 0 ? 'warning' : 'primary' );
+        return badgeClasses;
+    }
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count
+    }
+}
+ 
+export default Counter;
